6c15a1abd19e95e0d4cf3cfdce5a2a3e9aee192c
bsmiller25 <bsmiller25@gmail.com>
2016-01-20 14:55:25 -0500

    Update outlinebarplot.R

--- R/outlinebarplot.R
+++ R/outlinebarplot.R
@@ -1,5 +1,5 @@
 outlinebarplot <-
-function(bars,datavector,lty=5,lwd=1.5,col="black"){
+function(bars,datavector,...){
     xloc <- c(rep(0,length(bars)+1))
     for(i in 2:length(xloc)){
       xloc[i] <- bars[i-1]+(bars[i]-bars[i-1])/2
@@ -11,9 +11,6 @@ function(bars,datavector,lty=5,lwd=1.5,col="black"){
     lines(x=outlinebarplotdata$xloc,
           y=outlinebarplotdata$yloc,
           type='s',
-          lty=lty,
-          lwd=lwd,
-          col=col)
-    segments(x0 = xloc[1], y0 = 0, y1 = yloc[1],
-             lwd=lwd,lty=lty,col=col)
+          ...)
+    segments(x0 = xloc[1], y0 = 0, y1 = yloc[1],...)
   }
