94647bfafd5b8a396a0aeb373553bd286edd9fe4
bsmiller25 <bsmiller25@gmail.com>
2016-01-20 14:53:39 -0500

    Update rowmatch.R

--- R/rowmatch.R
+++ R/rowmatch.R
@@ -1,13 +1,20 @@
 rowmatch <-
-function(x, y, x.cols = colnames(x), y.cols = x.cols,
-  matches = "keep"){  
-  x$rowkeyname7 <- do.call(paste, c(x[,c(x.cols)], sep = ".key."))
-  y$rowkeyname7 <- do.call(paste, c(y[,c(y.cols)], sep = ".key."))
+function(x, y, x.cols = colnames(x), y.cols = x.cols, matches =
+           c("keep","drop")){
+  matches <- match.arg(matches)
+
+  if("row.key.name" %in% colnames(x) |
+     "row.key.name" %in% colnames(y) ){
+    stop("row.key.name is reserved. Please remove it from your column names")
+  }
+  
+  x$row.key.name <- do.call(paste, c(x[,c(x.cols)], sep = ".key."))
+  y$row.key.name <- do.call(paste, c(y[,c(y.cols)], sep = ".key."))
   if(matches == "keep"){
-    new <- x[x$rowkeyname7 %in% y$rowkeyname7,]
+    new <- x[x$row.key.name %in% y$row.key.name,]
   }
   if(matches == "drop"){
-    new <- x[!(x$rowkeyname7 %in% y$rowkeyname7),]
+    new <- x[!(x$row.key.name %in% y$row.key.name),]
   }
   return(new[,-c(length(colnames(new)))])
 }
